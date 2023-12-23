3904cf389b6eaedc9fc99363b84f83d0439c0bfd
NYBACHOK <nybachokua@gmail.com>
2023-12-23 17:38:14 +0200

    other opt

--- src/repository/merge.rs
+++ src/repository/merge.rs
@@ -13,7 +13,7 @@ impl<'a> MergeHelper<'a> {
     Self { repository }
   }
 
-  pub fn merge( &self, fail_on_conflict : bool, is_diff3 : bool, use_theirs : bool ) -> GitResult<()> {
+  pub fn merge( &self, allow_conflict : bool, fail_on_conflict : bool, is_diff3 : bool, use_theirs : bool ) -> GitResult<()> {
     let merge_from_name = format!("{UPSTREAM_NAME}/{}", self.repository.options.default_branch);
     let merge_from = self
       .repository
@@ -41,7 +41,7 @@ impl<'a> MergeHelper<'a> {
     let mut checkout_opt = CheckoutBuilder::default();
     checkout_opt.update_index(true);
     checkout_opt.use_theirs( use_theirs );
-    // checkout_opt.allow_conflicts(true);
+    checkout_opt.allow_conflicts( true );
     checkout_opt.remove_untracked(true);
 
     let mut merge_opt = MergeOptions::default();
